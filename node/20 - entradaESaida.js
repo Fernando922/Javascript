7a6858ae9ae9761973f1785232d143f4dc3c6e53
Fernando de Paula <di.paulaf@hotmail.com>
2020-03-25 08:44:29 -0300

    fim node

---
+++ node/20 - entradaESaida.js
@@ -0,0 +1,21 @@
+const anonimo = process.argv.indexOf('-a') !== -1
+// console.log(anonimo)
+
+
+if(anonimo){
+  process.stdout.write('Fala Anônimo!\n')
+  process.exit()
+}else{
+  process.stdout.write('Informe o seu nome: ')
+  process.stdin.on('data', data => {
+    const nome = data.toString().replace('\r\n', '')
+
+
+    process.stdout.write(`Fala ${nome}!!\n`)
+    process.exit()
+  })
+}
+
+
+//  \r\n pq o windows retorna o cursor para o inicio da linha, entaoo deve-se colocar
+// o \r tb para substituir isso!
